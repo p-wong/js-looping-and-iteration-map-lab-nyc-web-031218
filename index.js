c22dc111a027073fca05004979e0c1840149517b
p-wong <patriciawong08@gmail.com>
2018-04-23 15:15:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers (data) {
+  return data.map (function (driver) {
+    return driver.toLowerCase();
+  })
+}
+
+function nameToAttributes (data) {
+  return data.map (function (name) {
+    return { firstName: name.split(' ')[0], lastName: name.split(' ')[1]}
+  })
+}
+
+function attributesToPhrase (data) {
+  return data.map (function (name) {
+    return `${name.name} is from ${name.hometown}`
+  })
+}
